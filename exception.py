d34eb0ca479ec76fcee9577b0f18846ca15c125d
Vlad-Andreychik <andrieichik5@gmail.com>
2020-10-11 23:23:08 +0300

    Create exception.py

---
+++ exception.py
@@ -0,0 +1,38 @@
+import time
+
+
+def try_except():
+    try:
+        text = input('Введите что-нибудь --> ')
+    except EOFError:
+        print('Ну зачем вы сделали мне EOF?')
+    except KeyboardInterrupt:
+        print('Вы отменили операцию.')
+    else:
+        print('Вы ввели {0}'.format(text))
+
+
+def final():
+    try:
+        f = open('poem.txt')
+        while True:
+            line = f.readline()
+            if len(line) == 0:
+                break
+            print(line, end='')
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print('!! Вы отменили чтение файла.')
+
+    finally:
+        f.close()
+        print('(Очистка: Закрытие файла)')
+
+
+class ShortInputException(Exception):
+    '''Пользовательский класс исключения.'''
+
+    def __init__(self, length, atleast):
+        Exception.__init__(self)
+        self.length = length
+        self.atleast = atleast
