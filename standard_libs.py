4df122bbe3b90ae0ccb4cc4d92d4d0aaddc7d00f
Vlad-Andreychik <andrieichik5@gmail.com>
2020-10-11 23:50:33 +0300

    Update standard_libs.py

--- standard_libs.py
+++ standard_libs.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import platform
 import sys
 import warnings
 
@@ -9,6 +12,25 @@ def versioncheck():
                       RuntimeWarning)
     else:
         print('Нормальное продолжение')
-    
+
 
 versioncheck()
+
+
+def use_logging():
+    if platform.platform().startswith('Windows'):
+        logging_file = os.path.join(os.getenv('HOMEDRIVE'),
+                                    os.getenv('HOMEPATH'),
+                                    'test.log')
+    else:
+        logging_file = os.path.join(os.getenv('HOME'), 'test.log')
+    print("Сохраняем лог в", logging_file)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s : %(levelname)s : %(message)s',
+        filename=logging_file,
+        filemode='w',
+    )
+    logging.debug("Начало программы")
+    logging.info("Какие-то действия")
+    logging.warning("Программа умирает")
