c850b515d71230a63a04de4204e07eb585fe4685
Vlad-Andreychik <andrieichik5@gmail.com>
2020-10-11 15:48:50 +0300

    added backup_ver2

--- Tasks_13.py
+++ Tasks_13.py
@@ -3,11 +3,37 @@ import time
 
 
 def backup_ver1():
-    source = ['"C:\\My Documents"', 'G:\\kyrs']
+    source = ['C:\\Users', 'G:\\kyrs']
 
     target_dir = 'G:\\Backup'
 
     target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+    zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+    print(zip_command)
+    if os.system(zip_command) == 0:
+        print('Резервная копия успешно создана в', target)
+    else:
+        print('Создание резервной копии НЕ УДАЛОСЬ')
+
+
+def backup_ver2():
+    import os
+    import time
+
+    source = ['C:\\Users', 'G:\\kyrs']
+
+    target_dir = 'G:\\Backup'
+
+    today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+    now = time.strftime('%H%M%S')
+
+    if not os.path.exists(today):
+        os.mkdir(today)
+    print('Каталог успешно создан', today)
+
+    target = today + os.sep + now + '.zip'
 
     zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 
