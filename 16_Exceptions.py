23eefdf7386263ccdcca7899f6c6fc5e17b9a4cb
Vlad-Andreychik <andrieichik5@gmail.com>
2020-10-11 23:02:50 +0300

    Create 16_Exceptions.py

---
+++ 16_Exceptions.py
@@ -0,0 +1,9 @@
+def try_except():
+    try:
+        text = input('Введите что-нибудь --> ')
+    except EOFError:
+        print('Ну зачем вы сделали мне EOF?')
+    except KeyboardInterrupt:
+        print('Вы отменили операцию.')
+    else:
+        print('Вы ввели {0}'.format(text))
