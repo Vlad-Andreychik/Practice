50919877f0619182a0d7f1a16b5968eef5052765
Vlad-Andreychik <andrieichik5@gmail.com>
2020-10-11 16:00:52 +0300

    added backup_ver3

--- Tasks_13.py
+++ Tasks_13.py
@@ -41,3 +41,30 @@ def backup_ver2():
         print('Резервная копия успешно создана в', target)
     else:
         print('Создание резервной копии НЕ УДАЛОСЬ')
+
+
+def backup_ver3():
+    source = ['C:\\Users', 'G:\\kyrs']
+
+    target_dir = 'G:\\Backup'
+
+    today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+    now = time.strftime('%H%M%S')
+    comment = input('Введите комментарий --> ')
+    if len(comment) == 0:
+        target = today + os.sep + now + '.zip'
+    else:
+        target = today + os.sep + now + '_' + \
+                 comment.replace(' ', '_') + '.zip'
+
+    if not os.path.exists(today):
+        os.mkdir(today)
+    print('Каталог успешно создан', today)
+
+    zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+
+    if os.system(zip_command) == 0:
+        print('Резервная копия успешно создана в', target)
+    else:
+        print('Создание резервной копии НЕ УДАЛОСЬ')
