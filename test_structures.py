c01b68569790158909fdfd4bea0915d0ca973194
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-13 14:05:16 +0300

    Тесты для функции using_tuple

--- test_structures.py
+++ test_structures.py
@@ -4,3 +4,8 @@ import structures
 def test_shop():
     assert (structures.shop()) == ['манго', 'морковь', 'рис', 'яблоки']
     assert (len(structures.shop())) == 4
+
+
+def test_using_tuple():
+    assert (structures.using_tuple()) == ('обезьяна', 'верблюд', ('питон', 'слон', 'пингвин'))
+    assert (structures.using_tuple()[0:2]) == ('обезьяна', 'верблюд')
