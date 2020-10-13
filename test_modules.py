11b6a17581615b5d9d380d36a72350ad58fb5794
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-13 14:01:22 +0300

    Тесты для функции simple_or_not

--- test_modules.py
+++ test_modules.py
@@ -5,3 +5,10 @@ def test_imp_sys():
     a = modules.imp_sys()
     assert (a[0]) == 'C:\\Users\\vandreychyk\\PycharmProjects\\Practice'
     assert (len(a)) == 7
+
+
+def test_simple_or_not():
+    assert (modules.simple_or_not(5)) == [2, 3, 5, 7]
+    assert (modules.simple_or_not(-5)) == [2, 3]
+
+
