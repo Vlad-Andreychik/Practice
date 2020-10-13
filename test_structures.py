f11835ce2177903e821c8c811fb43c8df0b433d9
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-13 14:05:52 +0300

    Тесты для функции using_dict

--- test_structures.py
+++ test_structures.py
@@ -9,3 +9,8 @@ def test_shop():
 def test_using_tuple():
     assert (structures.using_tuple()) == ('обезьяна', 'верблюд', ('питон', 'слон', 'пингвин'))
     assert (structures.using_tuple()[0:2]) == ('обезьяна', 'верблюд')
+
+
+def test_using_dict():
+    assert (len(structures.using_dict())) == 4
+    assert (structures.using_dict()['Matsumoto']) == 'matz@ruby-lang.org'
