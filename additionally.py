f476b9a24e6e16523831cff99b81c6e2c98a2257
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-14 12:02:02 +0300

    Исправил тудушки

--- additionally.py
+++ additionally.py
@@ -1,4 +1,6 @@
 # TODO[agorozhanko 14.10.2020]:нужно заменить print на logger
+# TODO[vandreychyk 14.10.2020]: print на logger заменил
+import logging
 
 
 def get_error_details():
@@ -6,21 +8,26 @@ def get_error_details():
 
 
 def using_lambda():
+    logger = logging.getLogger("exampleApp.additionally.using_lambda")
     points = [{'x': 2, 'y': 3}, {'x': 4, 'y': 1}]
     points.sort(key=lambda i: i['y'])
-    print(points)
+    logger.info(points)
 
 
 def list_comprehension():
+    logger = logging.getLogger("exampleApp.additionally.list_comprehension")
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    listone = [2, 3, 4]
+    # TODO[vandreychyk 14.10.2020]: название исправлено
+    list_one = [2, 3, 4]
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    listtwo = [2 * i for i in listone if i > 2]
-    print(listtwo)
+    # TODO[vandreychyk 14.10.2020]: название исправлено
+    list_two = [2 * i for i in list_one if i > 2]
+    logger.info(list_two)
 
 
 # TODO[agorozhanko 14.10.2020]: название нужно исправить
-def powersum(power, *args):
+# TODO[vandreychyk 14.10.2020]: название исправлено
+def power_sum(power, *args):
     total = 0
     for i in args:
         total += pow(i, power)
@@ -29,8 +36,9 @@ def powersum(power, *args):
 
 def using_assert():
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    mylist = ['item']
-    assert len(mylist) >= 1
-    print(mylist.pop())
-    print(mylist)
-    assert len(mylist) >= 1
+    # TODO[vandreychyk 14.10.2020]: название исправлено
+    my_list = ['item']
+    assert len(my_list) >= 1
+    print(my_list.pop())
+    print(my_list)
+    assert len(my_list) >= 1
