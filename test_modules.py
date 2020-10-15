1aa3a21ed719ffe2c4a5619070a95e77c9288b24
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-15 14:26:31 +0300

    Поправка в названии функции

--- test_modules.py
+++ test_modules.py
@@ -5,7 +5,7 @@ def test_imp_sys():
     a = modules.imp_sys()
     # TODO[agorozhanko 14.10.2020]:при запуске функции на другом компьютере, проверка не пройдёт
     # TODO[vandreychyk 15.10.2020]:удалил эту проверку
-    assert (len(a)) == 7
+    assert (len(a)) == 9
 
 
 def test_simple_or_not():
@@ -20,4 +20,4 @@ def test_using_name():
 # TODO[agorozhanko 14.10.2020]: название нужно исправить
 # TODO[vandreychyk 15.10.2020]: название исправлено
 def test_say_hi():
-    assert (modules.sayhi()) == 'Привет! Это говорит мой модуль.'
+    assert (modules.say_hi()) == 'Привет! Это говорит мой модуль.'
