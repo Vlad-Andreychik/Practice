c828e533d8cb0cc93663a0bfbfcc7dc7c2eef675
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-15 13:38:07 +0300

    Переделал примеры в функции

--- greeting.py
+++ greeting.py
@@ -1,22 +1,26 @@
 import logging
 
 logger = logging.getLogger(__name__)
-# TODO[agorozhanko 14.10.2020]:нужно заменить print на logger
-# TODO[vandreychyk 15.10.2020]: заменил
-logger.info('Hello world')
 
-logger.info('''Это многострочная строка. Это её первая строка.
-Это её вторая строка.
-"What's your name?", - спросил я.
-Он ответил: "Bond, James Bond."
-''')
-age = 21
-name = 'Vlad'
-logger.info('Возраст {0} -- {1} лет'.format(name, age))
-i = 5
-logger.info(i)
-i = i + 1
-logger.info(i)
-a = 2
-a = a * 3
-logger.info(a)
+
+def greeting():
+    # TODO[agorozhanko 14.10.2020]:нужно заменить print на logger
+    # TODO[vandreychyk 15.10.2020]: заменил
+    logger.info('Hello world')
+
+    logger.info('''Это многострочная строка. Это её первая строка.
+                Это её вторая строка.
+                "What's your name?", - спросил я.
+                Он ответил: "Bond, James Bond."
+                ''')
+
+    age = 21
+    name = 'Vlad'
+    logger.info('Возраст {0} -- {1} лет'.format(name, age))
+    i = 5
+    logger.info(i)
+    i = i + 1
+    logger.info(i)
+    a = 2
+    a = a * 3
+    logger.info(a)
