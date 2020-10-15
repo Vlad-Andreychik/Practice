d94e7b54f3ab1f1f1c997806d2cee4f8d6d1921b
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-15 11:21:23 +0300

    Исправление todo в structures

--- structures.py
+++ structures.py
@@ -3,29 +3,31 @@
 
 def shop():
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    shoplist = ['яблоки', 'манго', 'морковь', 'бананы']
+    # TODO[vandreychyk 15.10.2020]: название исправлено
+    shop_list = ['яблоки', 'манго', 'морковь', 'бананы']
 
-    print('Я должен сделать', len(shoplist), 'покупки: ')
+    print('Я должен сделать', len(shop_list), 'покупки: ')
 
     print('Покупки: ', end=' ')
-    for item in shoplist:
+    for item in shop_list:
         print(item, end=' ')
 
     print('\nТакже нужно купить риса.')
-    shoplist.append('рис')
-    print('Теперь мой список покупок таков: ', shoplist)
+    shop_list.append('рис')
+    print('Теперь мой список покупок таков: ', shop_list)
 
     print('Отсортирую-ка свой список')
-    shoplist.sort()
-    print('Отсортированный список покупок выглядит так: ', shoplist)
+    shop_list.sort()
+    print('Отсортированный список покупок выглядит так: ', shop_list)
 
-    print('Первое, что мне нужно купить, это', shoplist[0])
+    print('Первое, что мне нужно купить, это', shop_list[0])
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    olditem = shoplist[0]
-    del shoplist[0]
-    print('Я купил', olditem)
-    print('Теперь мой список покупок: ', shoplist, '\n')
-    return shoplist
+    # TODO[vandreychyk 15.10.2020]: название исправлено
+    old_item = shop_list[0]
+    del shop_list[0]
+    print('Я купил', old_item)
+    print('Теперь мой список покупок: ', shop_list, '\n')
+    return shop_list
 
 
 def using_tuple():
@@ -67,48 +69,51 @@ def using_dict():
 
 def seq():
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    shoplist = ['яблоки', 'манго', 'морковь', 'бананы']
+    # TODO[vandreychyk 15.10.2020]: название исправлено
+    shop_list = ['яблоки', 'манго', 'морковь', 'бананы']
     name = 'swaroop'
 
-    print('Элемент 0 -', shoplist[0])
-    print('Элемент 1 -', shoplist[1])
-    print('Элемент 2 -', shoplist[2])
-    print('Элемент 3 -', shoplist[3])
-    print('Элемент -1 -', shoplist[-1])
-    print('Элемент -2 -', shoplist[-2])
+    print('Элемент 0 -', shop_list[0])
+    print('Элемент 1 -', shop_list[1])
+    print('Элемент 2 -', shop_list[2])
+    print('Элемент 3 -', shop_list[3])
+    print('Элемент -1 -', shop_list[-1])
+    print('Элемент -2 -', shop_list[-2])
     print('Символ 0 -', name[0])
 
-    print('Элементы с 1 по 3:', shoplist[1:3])
-    print('Элементы с 2 до конца:', shoplist[2:])
-    print('Элементы с 1 по -1:', shoplist[1:-1])
-    print('Элементы от начала до конца:', shoplist[:])
+    print('Элементы с 1 по 3:', shop_list[1:3])
+    print('Элементы с 2 до конца:', shop_list[2:])
+    print('Элементы с 1 по -1:', shop_list[1:-1])
+    print('Элементы от начала до конца:', shop_list[:])
 
     print('Символы с 1 по 3:', name[1:3])
     print('Символы с 2 до конца:', name[2:])
     print('Символы с 1 до -1:', name[1:-1])
     print('Символы от начала до конца:', name[:])
-    return shoplist
+    return shop_list
 
 
 def reference():
     print('Простое присваивание')
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    shoplist = ['яблоки', 'манго', 'морковь', 'бананы']
+    # TODO[vandreychyk 15.10.2020]: название исправлено
+    shop_list = ['яблоки', 'манго', 'морковь', 'бананы']
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    mylist = shoplist
+    # TODO[vandreychyk 15.10.2020]: название исправлено
+    my_list = shop_list
 
-    del shoplist[0]
+    del shop_list[0]
 
-    print('shoplist:', shoplist)
-    print('mylist:', mylist)
+    print('shoplist:', shop_list)
+    print('mylist:', my_list)
 
     print('Копирование при помощи полной вырезки')
-    mylist = shoplist[:]
-    del mylist[0]
+    my_list = shop_list[:]
+    del my_list[0]
 
-    print('shoplist:', shoplist)
-    print('mylist:', mylist)
-    return mylist, shoplist
+    print('shoplist:', shop_list)
+    print('mylist:', my_list)
+    return my_list, shop_list
 
 
 def str_methods():
@@ -125,6 +130,7 @@ def str_methods():
 
     delimiter = '_*_'
     # TODO[agorozhanko 14.10.2020]: название нужно исправить
-    mylist = ['Бразилия', 'Россия', 'Индия', 'Китай']
-    print(delimiter.join(mylist))
-    return name, mylist
+    # TODO[vandreychyk 15.10.2020]: название исправлено
+    my_list = ['Бразилия', 'Россия', 'Индия', 'Китай']
+    print(delimiter.join(my_list))
+    return name, my_list
