f7f84b14e27427f299da316cb292aaae946b9d14
Vlad-Andreychik <andrieichik5@gmail.com>
2020-10-18 23:31:35 +0300

    Создание модуля строк

---
+++ data_examples/string_1.py
@@ -0,0 +1,46 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+
+file_handler = logging.FileHandler('logs//string.log', mode='w')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
+def reverse(text):
+    return text[::-1]
+
+
+def unrepeated_letters(text):
+    word = ''
+    for i in text:
+        letter = i
+        if letter not in word and letter != ' ':
+            word += letter
+    return word
+
+
+def count_words(text):
+    text = text.split()
+    return len(text)
+
+
+def change_piece(text, old, new):
+    i = text.find(old)
+    text = text[0:i] + new + text[i + len(old):]
+    return text
+
+
+def sum_numbers(text):
+    num = 0
+    for number in text:
+        try:
+            if number.isdigit():
+                num += int(number)
+        except ValueError:
+            continue
+    return num
