810701b57a912c4d79aaf11f3396fa65a34d7201
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-19 13:55:15 +0300

    Написал по три теста на семь функций из модуля strings.py

--- data_examples/test_strings.py
+++ data_examples/test_strings.py
@@ -42,8 +42,44 @@ def test_change_piece_empty_new_piece():
 
 
 def test_change_piece_empty_old_piece():
-    assert (strings.change_piece('application', 'h', 'tok')) == 'applicatiotokapplication'
+    assert (strings.change_piece('application', 'h', 'tok')) == 'applicatiotok'
 
 
 def test_change_piece_normal_input():
     assert (strings.change_piece('application', 'appli', 'californi')) == 'californication'
+
+
+def test_sum_numbers_digits_only():
+    assert (strings.sum_numbers('45')) == 9
+
+
+def test_sum_numbers_without_digits():
+    assert (strings.sum_numbers('asdf')) == 0
+
+
+def test_sum_numbers_letters_and_digits():
+    assert (strings.sum_numbers('as3df5  7')) == 15
+
+
+def test_reverse_words_positions_one_word():
+    assert (strings.reverse_words_positions('word')) == 'word'
+
+
+def test_reverse_words_positions_sentence():
+    assert (strings.reverse_words_positions('Hello world')) == 'world Hello'
+
+
+def test_reverse_words_positions_empty():
+    assert (strings.reverse_words_positions('')) == ''
+
+
+def test_delete_spaces_in_the_start():
+    assert (strings.delete_spaces('    word')) == 'word'
+
+
+def test_delete_spaces_in_the_end():
+    assert (strings.delete_spaces('word    ')) == 'word'
+
+
+def test_delete_spaces_everywhere():
+    assert (strings.delete_spaces('   Hello   world     ')) == 'Hello world'
