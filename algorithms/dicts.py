08ca2a108ef361476731a3487aa93a17573f7926
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-22 15:38:10 +0300

    исправил стрингдоки и функцию my_print

--- algorithms/dicts.py
+++ algorithms/dicts.py
@@ -1,12 +1,12 @@
-keys = []  # Необходимо создание пустого списка и словаря для функции my_print
-new_dict = {}
-
-
 def count_it(text):
     """Дана строка.
 
     Возвращает словарь, в котором ключи - элементы строки,
-    а значения - количество этих элементов в данной строке"""
+    а значения - количество этих элементов в данной строке
+    :param text: строка
+    :type text: str
+    :return: Словарь
+    :rtype: dict"""
     return {i: text.count(i) for i in text}
 
 
@@ -20,6 +20,12 @@ def max_dct(first_dict, second_dict):
     то просто переносим со своим значением в новый словарь.
 
     Возвращает словарь
+    :param first_dict: Первый словарь
+    :type first_dict: dict
+    :param second_dict: Второй словарь
+    :type second_dict: dict
+    :return: новый словарь
+    :rtype: dict
     """
     third_dict = {}
     for first_key in list(first_dict.keys()):
@@ -41,12 +47,28 @@ def max_dct(first_dict, second_dict):
 def three_max_keys(dictionary):
     """Принимает словарь
 
-    Сортирует его по значениям по убыванию
+    Сортирует его по значениям по убыванию.
+    При введении значений типа отличного от int будет возвращаться пустой словарь.
 
-    Возвращает словарь с первыми тремя ключами отсортированного словаря"""
-    list_dict = list(dictionary.items())
-    list_dict.sort(key=lambda elem: elem[1], reverse=True)
-    return dict(list_dict[:3])
+    Возвращает словарь с первыми тремя ключами отсортированного словаря
+    :param dictionary: словарь
+    :type dictionary: dict
+    :return: словарь с тремя ключами с наибольшим значением
+    :rtype: dict
+    """
+    flag = True
+    for value in dictionary.values():
+        if isinstance(value, int):
+            flag = True
+        else:
+            flag = False
+            break
+    if flag:
+        list_dict = list(dictionary.items())
+        list_dict.sort(key=lambda elem: elem[1], reverse=True)
+        return dict(list_dict[:3])
+    else:
+        return {}
 
 
 def my_print(dictionary):
@@ -59,23 +81,44 @@ def my_print(dictionary):
     составленный из родительских ключей из начального словаря,
     разделенных "/".
 
-    Возвращает словарь"""
-    for k, v in dictionary.items():
-        if isinstance(v, dict):
-            keys.append(k)
-            my_print(v)
+    Возвращает словарь
+    :param dictionary: словарь, возможно с вложенными словарями
+    :type dictionary: dict
+    :return: словарь без вложенных словарей
+    :rtype: dict
+    """
+    new_dict = {}
+    for key, value in dictionary.items():
+        if not isinstance(value, dict) or (not value):
+            new_dict[key] = value or ''
         else:
-            keys.append(k)
-            new_key = '/'.join(keys)
-            new_dict[new_key] = v
+            for suffix, n_value in my_print(value).items():
+                r_key = key + '/' + suffix
+                new_dict[r_key] = n_value
     return new_dict
 
 
 def multiple_values(dictionary):
     """Дан словарь со значениями в виде чисел.
 
-    Возвращает произведение значений"""
-    result = 1
-    for key in dictionary:
-        result *= dictionary[key]
-    return result
+    Возвращает произведение значений.
+    При введении значений типа отличного от int будет возвращаться 0.
+    :param dictionary: словарь
+    :type dictionary: dict
+    :return: произведение значений словаря
+    :rtype: int
+    """
+    flag = False
+    for value in dictionary.values():
+        if isinstance(value, int):
+            flag = True
+        else:
+            flag = False
+            break
+    if flag:
+        result = 1
+        for key in dictionary:
+            result *= dictionary[key]
+        return result
+    else:
+        return 0
