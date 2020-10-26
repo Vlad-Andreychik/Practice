108259a52454726ebef4ae8edc16d9f18d34c473
Vlad Andreychik <andrieichik5@gmail.com>
2020-10-26 16:19:41 +0300

    Сделал оставшиеся три примера на кортежи

--- algorithms/tuples.py
+++ algorithms/tuples.py
@@ -15,8 +15,11 @@ def above_average(tupl):
     """
     Принимает кортеж, в котором каждый элемент содержит имя и возраст.
     Возвращает кортеж с элементами, чей возраст выше среднего.
+
     :param tupl: кортеж
+    :type tupl: tuple
     :return: кортеж
+    :rtype: tuple
     """
     average = 0
     list_above_average = []
@@ -35,9 +38,13 @@ def union_tuples(first_tuple, second_tuple):
     """
     Принимает два кортежа. Создает третий кортеж путем объединения первых двух.
     Возвращает третий кортеж и количество нулей в нем
+
     :param first_tuple: кортеж
+    :type first_tuple: tuple
     :param second_tuple: кортеж
-    :return: кортеж, число
+    :type second_tuple: tuple
+    :return: кортеж
+    :rtype: tuple
     """
     new_tuple = []
     try:
@@ -47,3 +54,75 @@ def union_tuples(first_tuple, second_tuple):
     except IndexError:
         logger.warning('Неправильный ввод')
     return tuple(new_tuple), new_tuple.count(0)
+
+
+def slicer(tupl, element):
+    """
+    Принимает кортеж и случайный элемент. Возвращает новый кортеж,
+    начинающийся с первого появления элемента в нем и
+    заканчивающийся вторым его появлением включительно.
+    Если элемента нет вовсе – вернуть пустой кортеж.
+
+    :param tupl: кортеж
+    :type tupl: tuple
+    :param element: элемент
+    :type element: Any
+    :return: кортеж
+    :rtype:tuple
+    """
+    try:
+        if tupl.count(element) > 1:
+            tupl_1 = tupl[tupl.index(element) + 1:]
+            return tupl[tupl.index(element):tupl.index(element) + tupl_1.index(element) + 2]
+        elif tupl.count(element) == 1:
+            return tupl[tupl.index(element):]
+        else:
+            return ()
+    except ValueError:
+        logger.warning('Неправильный ввод')
+    except TypeError:
+        logger.warning('Неправильный ввод')
+
+
+def sieve(list_of_integers):
+    """
+    На вход поступает список целых чисел.
+    В результате выполнения этой функции будет получен кортеж
+    уникальных элементов списка в обратном порядке.
+
+    :param list_of_integers: список целых чисел
+    :type list_of_integers: list
+    :return: кортеж
+    :rtype: tuple
+    """
+    try:
+        for i in list_of_integers:
+            if not isinstance(i, int):
+                raise ValueError
+        set_of_list = set(list_of_integers)
+        return tuple(sorted(set_of_list))
+    except (ValueError, TypeError):
+        logger.warning('Неправильный ввод')
+
+
+def del_from_tuple(tupl, element):
+    """
+    На вход поступает кортеж и элемент.
+    Функция удаляет первое появление определенного элемента из кортежа по значению
+    и возвращать кортеж без оного.
+
+     :param tupl: кортеж
+     :type tupl: tuple
+     :param element: элемент
+     :type element: Any
+     :return: кортеж
+     :rtype: tuple
+     """
+    try:
+        if element not in tupl:
+            return tupl
+        list_of_tuple = list(tupl)
+        list_of_tuple.remove(element)
+        return tuple(list_of_tuple)
+    except (ValueError, TypeError):
+        logger.warning('Неправильный ввод')
