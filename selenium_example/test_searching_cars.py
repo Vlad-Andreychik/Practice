041b367fb5105558808131320141a35a28513f3d
Vlad Andreychik <andrieichik5@gmail.com>
2020-11-04 16:14:53 +0300

    Поменял проверку в корректном поиске, добавил фикстуру закрытия браузера и возвращения на пред страницу

--- selenium_example/test_searching_cars.py
+++ selenium_example/test_searching_cars.py
@@ -1,3 +1,5 @@
+import pytest
+
 import selenium_example.xpath as xpath
 from selenium_example.av_by_utils import search_auto
 from selenium_example.selenium_utils import wait_element, get_driver, go_to_url
@@ -6,11 +8,26 @@ driver = get_driver('chrome')
 go_to_url(driver)
 
 
-def test_searching_cars_with_model():
+@pytest.fixture()
+def closing_browser():
+    yield
+    driver.quit()
+
+
+@pytest.fixture()
+def back_page():
+    yield
+    driver.back()
+    driver.refresh()
+
+
+def test_searching_cars_with_model(back_page):
     search_auto(driver, brand='Audi', model='TT')
-    assert wait_element(driver, xpath.LISTING_TITLE)
+    listing_container = wait_element(driver, xpath.LISTING_CONTAINER)
+    listing_items = listing_container.find_elements_by_xpath(xpath.LISTING_ITEMS)
+    assert len(listing_items) > 0
 
 
-def test_searching_cars_wrong_year_from():
+def test_searching_cars_wrong_year_from(closing_browser):
     search_auto(driver, brand='Audi', model='100', year_from='2000')
     assert wait_element(driver, xpath.EMPTY_SEARCH)
