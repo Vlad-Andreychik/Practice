98204f8f85a24545b16e240ecc06b9dd83b8e064
Vlad Andreychik <andrieichik5@gmail.com>
2020-11-05 16:17:31 +0300

    Сделал проверку пустого результата поиска на текст

--- selenium_example/test_searching_cars.py
+++ selenium_example/test_searching_cars.py
@@ -1,35 +1,16 @@
-import pytest
-
 import selenium_example.xpath as xpath
 from selenium_example.av_by_utils import search_auto
-from selenium_example.selenium_utils import wait_element, get_driver, go_to_url
-
-driver = get_driver('chrome')
-go_to_url(driver)
+from selenium_example.selenium_utils import wait_element
 
 
 # TODO[agorozhanko 05.11.2020]:где должны находится фикстуры?
-@pytest.fixture()
-def closing_browser():
-    """
-    Фикстура закрывает браузер после выполнения всех тестов в данном модуле
-    """
-    yield
-    driver.quit()
-
-
-@pytest.fixture()
-def back_page():
-    """
-    Фикстура возвращает предыдущую страницу и обновляет ее после выполнения теста, который использует ее
-    """
-    yield
-    driver.back()
-    driver.refresh()
+# TODO[vandreychyk 05.11.2020]: перенес в conftest.py
 
 
 # TODO[agorozhanko 05.11.2020]:где запуск браузера перед каждым тестом и закрытие после?
-def test_searching_cars_with_model(back_page):
+
+
+def test_searching_cars_with_model():
     """
     Тест поиска авто с корректным вводом данных
     """
@@ -39,9 +20,10 @@ def test_searching_cars_with_model(back_page):
     assert len(listing_items) > 0
 
 
-def test_searching_cars_wrong_year_from(closing_browser):
+def test_searching_cars_wrong_year_from():
     """
     Тест поиска авто с некорректным вводом данных(несуществующий год выпуска для данной модели марки)
     """
     search_auto(driver, brand='Audi', model='100', year_from='2000')
-    assert wait_element(driver, xpath.EMPTY_SEARCH)
+    element = wait_element(driver, xpath.EMPTY_SEARCH)
+    assert element.text == "Мы не нашли объявлений по вашему запросу"
