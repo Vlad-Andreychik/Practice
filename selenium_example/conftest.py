a303d1d22be5874eaa60748eda0bcd008939591d
Vlad Andreychik <andrieichik5@gmail.com>
2020-11-05 16:15:15 +0300

    Создал модуль для фикстур и перенес их туда

---
+++ selenium_example/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from selenium_example.selenium_utils import get_driver
+
+driver = get_driver('chrome')
+
+
+@pytest.fixture()
+def closing_browser():
+    """
+    Фикстура закрывает браузер после выполнения всех тестов в данном модуле
+    """
+    yield
+    driver.quit()
+
+
+@pytest.fixture()
+def back_page():
+    """
+    Фикстура возвращает предыдущую страницу и обновляет ее после выполнения теста, который использует ее
+    """
+    yield
+    driver.back()
+    driver.refresh()
